0605f3b6ffacd7f2eac78524a99337b7cf1a058b
Marcos Acosta <m@mmaa.co>
2017-08-12 08:50:41 -0500

    Use exa instead of ls

--- fish/config.fish
+++ fish/config.fish
@@ -75,9 +75,9 @@ function gpl; git pull --prune                                  ; end
 function gps; git push $argv                                    ; end
 function gpz; gpl; and gps                                      ; end
 function gs;  git status                                        ; end
-function l;   ls -F -1 $argv                                    ; end
-function la;  ls -F -1 -A $argv                                 ; end
-function ll;  ls -F -A -o -h $argv                              ; end
+function l;   exa $argv                                         ; end
+function lt;  exa -T $argv                                      ; end
+function ll;  exa -l -a $argv                                   ; end
 
 set -g -x EDITOR 'nvim'
 set -g -x PGDATA '/usr/local/var/postgres/'
