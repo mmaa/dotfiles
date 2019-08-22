04221e3f33625a95688492420227f4c7780b1b7e
Marcos Acosta <m@mmaa.co>
2019-08-22 13:59:43 -0500

    Update fish prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -2,9 +2,9 @@ function fish_right_prompt
   # git
   set -l index (git status --porcelain 2>/dev/null)
   if test -z "$index"
-    set_color green
+    set_color --bold green
   else
-    set_color yellow
+    set_color --bold yellow
   end
   echo -n (__fish_git_prompt | sed 's/[\(\)]//g')
   set_color normal
